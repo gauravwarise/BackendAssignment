497693aebe1c21f9f39e4e59f074cd89be3a8dfe
gauravwarise <gauravwarise04@gmail.com>
2024-03-11 23:48:44 +0530

    remove unused folder

--- Backend_Assignment_Onefin/tests/accounts/test_models.py
+++
@@ -1,5 +0,0 @@
-class TestAuthUserModel:
-    def test_str_method(self, authuser_factory):
-        x = authuser_factory() 
-
-        assert x.__str__() == "test_authuser"
--- Backend_Assignment_Onefin/tests/factories.py
+++
@@ -1,9 +0,0 @@
-import factory
-
-from apps.accounts.models import AuthUser
-
-
-class AuthUserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = AuthUser
-    name = "test_authuser" 
